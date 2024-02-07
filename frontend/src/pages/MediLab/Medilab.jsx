4bf597e731e4673e616299e66b68c1708fdbbde8
Tareq57 <tareq57@gmail.com>
2024-02-08 01:25:44 +0600

    MediLab Page Modified

--- frontend/src/pages/MediLab/Medilab.jsx
+++ frontend/src/pages/MediLab/Medilab.jsx
@@ -96,179 +96,226 @@ const Medilab = () => {
 
   // console.log(state);
 
+  // return (
+  //   <div className="mx-[180px] mt-[40px] flex">
+  //     <div className="flex flex-col w-2/3">
+  //       <div className="flex justify-between">
+  //         <h1 className="font-bold text-3xl text">All Lab Test</h1>
+  //         <div className="flex">
+  //           <input
+  //             type="text"
+  //             placeholder="Search by name"
+  //             value={search.name}
+  //             onChange={(e) => handleChange("name", e.target.value)}
+  //             className="border-b-2 border-black focus:outline-none w-[300px]"
+  //           />
+  //           <Button
+  //             size="icon"
+  //             className="rounded-full bg-orange-500 hover:bg-orange-600"
+  //           >
+  //             <FaSearch />
+  //           </Button>
+  //         </div>
+  //       </div>
+
+  //       {doctors.map((doctor, index) => (
+  //         <div
+  //           key={index}
+  //           className="flex my-[10px] h-[150px] rounded-lg border border-slate-400 overflow-hidden"
+  //         >
+  //           <div className="w-1/3 h-full">
+  //             <img
+  //               src={doctor.photo}
+  //               alt="Doctor"
+  //               className="top-0 left-0 w-full h-full object-cover"
+  //             />
+  //           </div>
+  //           <div className="w-2/3 p-[20px]">
+  //             <div className="flex justify-between">
+  //               <div>
+  //                 <h1 className="font-bold ">{doctor.name}</h1>
+  //                 <h1 className="font-bold text-xl">
+  //                   {doctor.specialization.name} Specialist
+  //                 </h1>
+  //               </div>
+  //               <div className="flex space-x-1 items-center justify-center">
+  //                 <img src={AvgStar} className="w-[30px] h-[30px]" alt="" />
+  //                 {/* <p className="font-bold pt-2">{doctor.averageStars.toFixed(2)}</p> */}
+  //               </div>
+  //             </div>
+  //             <div className="flex my-[10px]">
+  //               <div className="flex mr-[10px]">
+  //                 <PiClockCountdownFill className="text-orange-400 " />
+  //                 <p className="text-xs"> {doctor.patientCount} Patients</p>
+  //               </div>
+  //               <div className="flex mx-[10px]">
+  //                 <TbCalendarStats className="text-orange-400 " />
+  //                 <p className="text-xs">
+  //                   {" "}
+  //                   Joined on {doctor.createdAt.split("T")[0]}{" "}
+  //                 </p>
+  //               </div>
+  //               <div className="flex mx-[10px]">
+  //                 <TbDeviceWatchStats2 className="text-orange-400 " />
+  //                 <p className="text-xs"> {doctor.slotCount} slots available</p>
+  //               </div>
+  //             </div>
+  //             <hr className="border-gray-200" />
+  //             <div className="flex justify-between my-[10px]">
+  //               <h1 className="text-red-500 font-extrabold ">
+  //                 Fee : {doctor.fee} Taka
+  //               </h1>
+  //               <h1 className="font-bold hover:scale-110 transition-transform">
+  //                 <Link to={`/doctors/${doctor._id}`}>view more</Link>
+  //               </h1>
+  //             </div>
+  //           </div>
+  //         </div>
+  //       ))}
+  //     </div>
+
+  //     <div className="flex flex-col w-1/3 container space-y-4">
+  //       <h1 className="font-bold text-lg">Specialization</h1>
+  //       <RadioGroup
+  //         defaultValue="comfortable"
+  //         value={search.specialization}
+  //         onValueChange={(value) => handleChange("specialization", value)}
+  //       >
+  //         <div className="flex items-center space-x-2">
+  //           <RadioGroupItem value={"all"} id="r1" />
+  //           <Label htmlFor="r1">All</Label>
+  //         </div>
+  //         {specialization.map((sp, index) => (
+  //           <div key={index} className="flex items-center space-x-2">
+  //             <RadioGroupItem value={sp._id} id="r1" />
+  //             <Label htmlFor="r1">{sp.name}</Label>
+  //           </div>
+  //         ))}
+  //       </RadioGroup>
+
+  //       <h1 className="font-bold text-lg">Slot Availability</h1>
+
+  //       <RadioGroup
+  //         defaultValue="comfortable"
+  //         value={search.timerange}
+  //         onValueChange={(value) => handleChange("timerange", value)}
+  //       >
+  //         <div className="flex items-center space-x-2">
+  //           <RadioGroupItem value="all" id="r1" />
+  //           <Label htmlFor="r1">All</Label>
+  //         </div>
+  //         <div className="flex items-center space-x-2">
+  //           <RadioGroupItem value="today" id="r2" />
+  //           <Label htmlFor="r2">Today</Label>
+  //         </div>
+  //         <div className="flex items-center space-x-2">
+  //           <RadioGroupItem value="week" id="r3" />
+  //           <Label htmlFor="r3">This week</Label>
+  //         </div>
+  //       </RadioGroup>
+
+  //       <h1 className="font-bold text-lg">Fee</h1>
+
+  //       <div className="flex flex-col space-y-2">
+  //         <h1 className="font-bold text-lg">
+  //           {search.feeLower}-{search.feeUpper}
+  //         </h1>
+
+  //         <Slider
+  //           min={0}
+  //           max={1000}
+  //           value={[search.feeUpper]}
+  //           className="w-[200px]"
+  //           step={100}
+  //           onValueChange={(value) => {
+  //             setSearch({
+  //               ...search,
+  //               feeUpper: value[0],
+  //             });
+  //           }}
+  //         />
+  //       </div>
+
+  //       <h1 className="font-bold text-lg">Review</h1>
+
+  //       <RadioGroup
+  //         defaultValue="comfortable"
+  //         value={search.rating.toString()}
+  //         onValueChange={(value) => handleChange("rating", value)}
+  //       >
+  //         <div className="flex items-center space-x-2">
+  //           <RadioGroupItem value="5" id="r1" />
+  //           <Label htmlFor="r1">5 star</Label>
+  //         </div>
+  //         <div className="flex items-center space-x-2">
+  //           <RadioGroupItem value="4" id="r2" />
+  //           <Label htmlFor="r2">4+ star</Label>
+  //         </div>
+  //         <div className="flex items-center space-x-2">
+  //           <RadioGroupItem value="3" id="r3" />
+  //           <Label htmlFor="r3">3+ star</Label>
+  //         </div>
+  //         <div className="flex items-center space-x-2">
+  //           <RadioGroupItem value="2" id="r3" />
+  //           <Label htmlFor="r3">2+ star</Label>
+  //         </div>
+  //         <div className="flex items-center space-x-2">
+  //           <RadioGroupItem value="1" id="r3" />
+  //           <Label htmlFor="r3">1+ star</Label>
+  //         </div>
+  //         <div className="flex items-center space-x-2">
+  //           <RadioGroupItem value="0" id="r3" />
+  //           <Label htmlFor="r3">None</Label>
+  //         </div>
+  //       </RadioGroup>
+  //     </div>
+  //   </div>
+  // );
   return (
-    <div className="mx-[180px] mt-[40px] flex">
-      <div className="flex flex-col w-2/3">
-        <div className="flex justify-between">
-          <h1 className="font-bold text-3xl text">All Lab Test</h1>
-          <div className="flex">
-            <input
-              type="text"
-              placeholder="Search by name"
-              value={search.name}
-              onChange={(e) => handleChange("name", e.target.value)}
-              className="border-b-2 border-black focus:outline-none w-[300px]"
+    <div className="mx-4 my-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+      {doctors.map((doctor, index) => (
+        <div
+          key={index}
+          className="bg-white p-4 rounded-md shadow-md hover:shadow-lg transition duration-300 ease-in-out"
+        >
+          <div className="flex justify-center mb-2">
+            <img
+              src={doctor.photo}
+              alt="Doctor"
+              className="w-24 h-24 object-cover rounded-full"
             />
-            <Button
-              size="icon"
-              className="rounded-full bg-orange-500 hover:bg-orange-600"
-            >
-              <FaSearch />
-            </Button>
+          </div>
+          <div className="mb-2">
+            <h2 className="text-xl font-bold">{doctor.name}</h2>
+            <p className="text-gray-500">{doctor.specialization.name} Specialist</p>
+          </div>
+          <div className="flex items-center mb-2">
+            <img src={AvgStar} alt="Average Star" className="w-6 h-6 mr-2" />
+            <p className="font-bold">{doctor.averageStars.toFixed(2)}</p>
+          </div>
+          <div className="flex items-center mb-2">
+            <PiClockCountdownFill className="text-orange-400 mr-1" />
+            <p className="text-xs">{doctor.patientCount} Patients</p>
+          </div>
+          <div className="flex items-center mb-2">
+            <TbCalendarStats className="text-orange-400 mr-1" />
+            <p className="text-xs">Joined on {doctor.createdAt.split("T")[0]}</p>
+          </div>
+          <div className="flex items-center mb-2">
+            <TbDeviceWatchStats2 className="text-orange-400 mr-1" />
+            <p className="text-xs">{doctor.slotCount} slots available</p>
+          </div>
+          <hr className="border-gray-200 my-2" />
+          <div className="flex justify-between">
+            <h1 className="text-red-500 font-extrabold">
+              Fee: {doctor.fee} Taka
+            </h1>
+            <h1 className="font-bold hover:scale-110 transition-transform">
+              <Link to={`/doctors/${doctor._id}`}>View More</Link>
+            </h1>
           </div>
         </div>
-
-        {doctors.map((doctor, index) => (
-          <div
-            key={index}
-            className="flex my-[10px] h-[150px] rounded-lg border border-slate-400 overflow-hidden"
-          >
-            <div className="w-1/3 h-full">
-              <img
-                src={doctor.photo}
-                alt="Doctor"
-                className="top-0 left-0 w-full h-full object-cover"
-              />
-            </div>
-            <div className="w-2/3 p-[20px]">
-              <div className="flex justify-between">
-                <div>
-                  <h1 className="font-bold ">{doctor.name}</h1>
-                  <h1 className="font-bold text-xl">
-                    {doctor.specialization.name} Specialist
-                  </h1>
-                </div>
-                <div className="flex space-x-1 items-center justify-center">
-                  <img src={AvgStar} className="w-[30px] h-[30px]" alt="" />
-                  {/* <p className="font-bold pt-2">{doctor.averageStars.toFixed(2)}</p> */}
-                </div>
-              </div>
-              <div className="flex my-[10px]">
-                <div className="flex mr-[10px]">
-                  <PiClockCountdownFill className="text-orange-400 " />
-                  <p className="text-xs"> {doctor.patientCount} Patients</p>
-                </div>
-                <div className="flex mx-[10px]">
-                  <TbCalendarStats className="text-orange-400 " />
-                  <p className="text-xs">
-                    {" "}
-                    Joined on {doctor.createdAt.split("T")[0]}{" "}
-                  </p>
-                </div>
-                <div className="flex mx-[10px]">
-                  <TbDeviceWatchStats2 className="text-orange-400 " />
-                  <p className="text-xs"> {doctor.slotCount} slots available</p>
-                </div>
-              </div>
-              <hr className="border-gray-200" />
-              <div className="flex justify-between my-[10px]">
-                <h1 className="text-red-500 font-extrabold ">
-                  Fee : {doctor.fee} Taka
-                </h1>
-                <h1 className="font-bold hover:scale-110 transition-transform">
-                  <Link to={`/doctors/${doctor._id}`}>view more</Link>
-                </h1>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-
-      <div className="flex flex-col w-1/3 container space-y-4">
-        <h1 className="font-bold text-lg">Specialization</h1>
-        <RadioGroup
-          defaultValue="comfortable"
-          value={search.specialization}
-          onValueChange={(value) => handleChange("specialization", value)}
-        >
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value={"all"} id="r1" />
-            <Label htmlFor="r1">All</Label>
-          </div>
-          {specialization.map((sp, index) => (
-            <div key={index} className="flex items-center space-x-2">
-              <RadioGroupItem value={sp._id} id="r1" />
-              <Label htmlFor="r1">{sp.name}</Label>
-            </div>
-          ))}
-        </RadioGroup>
-
-        <h1 className="font-bold text-lg">Slot Availability</h1>
-
-        <RadioGroup
-          defaultValue="comfortable"
-          value={search.timerange}
-          onValueChange={(value) => handleChange("timerange", value)}
-        >
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="all" id="r1" />
-            <Label htmlFor="r1">All</Label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="today" id="r2" />
-            <Label htmlFor="r2">Today</Label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="week" id="r3" />
-            <Label htmlFor="r3">This week</Label>
-          </div>
-        </RadioGroup>
-
-        <h1 className="font-bold text-lg">Fee</h1>
-
-        <div className="flex flex-col space-y-2">
-          <h1 className="font-bold text-lg">
-            {search.feeLower}-{search.feeUpper}
-          </h1>
-
-          <Slider
-            min={0}
-            max={1000}
-            value={[search.feeUpper]}
-            className="w-[200px]"
-            step={100}
-            onValueChange={(value) => {
-              setSearch({
-                ...search,
-                feeUpper: value[0],
-              });
-            }}
-          />
-        </div>
-
-        <h1 className="font-bold text-lg">Review</h1>
-
-        <RadioGroup
-          defaultValue="comfortable"
-          value={search.rating.toString()}
-          onValueChange={(value) => handleChange("rating", value)}
-        >
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="5" id="r1" />
-            <Label htmlFor="r1">5 star</Label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="4" id="r2" />
-            <Label htmlFor="r2">4+ star</Label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="3" id="r3" />
-            <Label htmlFor="r3">3+ star</Label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="2" id="r3" />
-            <Label htmlFor="r3">2+ star</Label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="1" id="r3" />
-            <Label htmlFor="r3">1+ star</Label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="0" id="r3" />
-            <Label htmlFor="r3">None</Label>
-          </div>
-        </RadioGroup>
-      </div>
+      ))}
     </div>
   );
 };
